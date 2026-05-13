17c7271e1b11536768a4b2a2d461f0f42ed8c7c7
AbdulSalam <abdulsalam.0302@gmail.com>
2026-05-13 18:57:32 +0500

    code pushed

--- lib/features/auth/presentation/signin/views/signin_view.dart
+++ lib/features/auth/presentation/signin/views/signin_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get_utils/get_utils.dart';
 import 'package:iconsax/iconsax.dart';
-
 import '../../../../../core/app_dependencies.dart';
 import '../../../../../core/constants/const_exports.dart';
 import '../../../../../core/utils/utils_exports.dart';
