2d11961286768bb41670a0abecb5d7fd26db17ca
AbdulSalam <abdulsalam.0302@gmail.com>
2026-07-23 17:59:07 +0500

    code pushed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
     defaultConfig {
         applicationId = "com.manticerp.softronix"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
